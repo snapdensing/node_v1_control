0adf3a4f3cb5f78af92e9319577f66a836bc04bb
Snap <snapdensing@gmail.com>
2019-10-29 15:31:12 +0800

    Added shell script for setting node addresses to environment variables for easier command line use

---
+++ set_nodeaddr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+# Save node addresses to environment variables for easier configuration
+export UPPER=0013a200
+export NODE_PERCIVAL=$UPPER"4146ef5a"
+export NODE_LANCELOT=$UPPER"40bf1f4a"
+export NODE_MORDRED=$UPPER"4146f38d"
+export NODE_GALAHAD=$UPPER"41033e85"
