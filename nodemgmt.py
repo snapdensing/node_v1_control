76592741fcb134ab3139a2973cd7b4cfe09b9e5b
Snap <snapdensing@gmail.com>
2020-06-12 19:40:45 +0800

    Added ping, getAggre and getPeriod

--- nodemgmt.py
+++ nodemgmt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import command_api as c
+from datetime import datetime
 
 # Initialize XBee command node
 def initcmd(dev,channel):
@@ -30,17 +31,37 @@ class node:
     self.aggre = '0013a200409a0a81'
     self.txperiod = 10
     self.loc = 'noloc'
+    self.lastping = None
+    self.lastcommit = None
 
   def ping(self,ser):
     data = c.remote_query(ser,self.addr,'A')
-    print('Aggregator: {}'.format(parseAggre(data)))
+    try:
+      print('Aggregator: {}'.format(parseAggre(data)))
+      self.lastping = datetime.now()
+    except:
+      print('Error pinging node')
+      return None
 
   def getAggre(self,ser):
-    pass
+    payload = c.remote_query(ser,self.addr,'A')
+    try:
+      self.lastping = datetime.now()
+      return parseAggre(payload)
+    except:
+      print('Error getting aggregator address')
+      return None
 
   def getPeriod(self,ser):
-    pass
+    payload = c.remote_query(ser,self.addr,'T')
+    try:
+      self.lastping = datetime.now()
+      return parsePeriod(payload)
+    except:
+      print('Error getting transmit period')
+      return None
 
+# Parse Aggregator address from Query reply
 def parseAggre(payload):
 
   if len(payload) != 20:
@@ -51,3 +72,15 @@ def parseAggre(payload):
     return 'Error'
   else:
     return payload[2:]
+
+# Parse Period from Query reply
+def parsePeriod(payload):
+
+  if not ((len(payload) == 6) | (len(payload) == 8)):
+    print('Invalid payload: length')
+    return 'Error'
+  elif payload[0:4] != '5154':
+    print('Invalid payload: header')
+    return 'Error'
+  else:
+    return int('0x'+payload[4:],0)
