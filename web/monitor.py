260e2db71722ac31498cccd91cd5b9ab7ada9daa
Snap <snapdensing@gmail.com>
2020-07-09 17:46:22 +0800

    Initial version of monitor.py. Only pulls from REST API and only when run

---
+++ web/monitor.py
@@ -0,0 +1,58 @@
+import argparse
+import status_func as s
+
+# Configuration variables
+refresh_interval = '5m'
+gspread_creds = './gsheets/gspread_credentials.json'
+gspread_key = '1CyLKDCl3noYfy95CFcEfl-R1kQ_MnxuK1gpcpCrXyVU'
+gspread_worksheet = 'Monitor'
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("nodelist", help="File containing list of nodes and addresses")
+args = parser.parse_args()
+
+# Read file
+try:
+  init_list = {}
+  fp = open(args.nodelist,'r')
+  for line in fp:
+    # remove newline
+    x = line.split() 
+    # separate fields
+    y = x[0].split(',')
+
+    init_list[y[0]] = y[1].upper()
+
+  fp.close()
+
+except:
+  print('Error reading file')
+  quit()
+
+# Initialize spreadsheet
+worksheet = s.openWorksheet(gspread_key,gspread_creds,gspread_worksheet)
+data = [None for item in init_list]
+header = ['Name', 'Address', 'Status']
+worksheet.update('A2',[header])
+
+i = 0
+for item in init_list:
+  data[i] = [item, init_list[item]]
+  i = i + 1
+
+worksheet.update('A3',data)
+
+# Get Sensing nodes from REST API
+try:
+  nodes_sensing = s.getSensingRest(refresh_interval)
+except:
+  print('Error getting data from REST API')
+
+# Update spreadsheet with REST API results
+for item in nodes_sensing:
+  if item in init_list:
+    s.findUpdate(worksheet, item, nodes_sensing[item], 'Sensing')
+  else:
+    print('Node found sensing is not in initial list 0x{}'.format(
+      nodes_sensing[item]))
---
+++ web/status_func.py
@@ -0,0 +1,70 @@
+import requests
+import gspread
+from datetime import datetime
+import nodemgmt as n
+
+# Use RestAPI to get list of sensing nodes
+# Arguments:
+# - interval - (string) Interval for Rest API (ex. '5m', '1h')
+# Return:
+# - nodes_dict - dictionary of node address keyed by name(node ID)
+def getSensingRest(interval):
+
+  base_url = 'http://122.53.116.119/api-v0_5/aggregator/2/nodes/'
+  url = base_url + interval
+  print(url)
+
+  # Request
+  r = requests.get(url)
+  x = r.json()
+
+  # Node list
+  nodes_list = x['results']
+
+  # Node dictionary
+  # key: name
+  # value: address
+  nodes_dict = {}
+  for item in nodes_list:
+    nodes_dict[item['node_id']] = item['node_mac']
+
+  return nodes_dict
+
+# Open google sheet worksheet
+# Arguments:
+# - key - Google sheet URL id
+# - creds - JSON file for credentials
+# - worksheet - (String) Worksheet name
+# Returns:
+# - wkst - gspread worksheet object
+def openWorksheet(key,creds,worksheet):
+
+  gc = gspread.service_account(filename=creds)
+  sh = gc.open_by_key(key)
+  wkst = sh.worksheet(worksheet)
+
+  return wkst
+
+# Find node in worksheet and update status
+# Arguments:
+# - wkst - gspread worksheet object
+# - name - (String) node name (ID)
+# - addr - (String) Node address in hexadecimal
+# - status - (String) Status value to be used 
+def findUpdate(wkst,name,addr,status):
+
+  addr_cell = wkst.find(addr)
+  print(addr_cell)
+  row = addr_cell.row
+  col = addr_cell.col
+  print(addr_cell.value)
+
+  # Read adjacent cell (addr field)
+  name_cell = wkst.cell(row, col-1)
+  print(name_cell.value)
+
+  # Update status
+  if name_cell.value == name:
+    wkst.update_cell(row, col+1, status)
+    print('Node {} status updated to {}'.format(name_cell.value,
+      status))
